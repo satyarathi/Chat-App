f28d11df6ab013078d293e0fcff844357220eada
satyarathi <jitendrasatyarathi@gmail.com>
2023-04-19 15:50:33 +0530

    [Jitendra] : Added connection with backend for user registration

--- chat_frontend/src/Pages/signup/signup.jsx
+++ chat_frontend/src/Pages/signup/signup.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./signup.css";
 import Image from "../../image/chat.png";
 import Button from "@mui/material/Button";
+import { registerUser } from "../../service/userservice";
+
+const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+const nameReg = /^[A-Z]{1}[a-zA-Z]+$/;
 
 function SignUp(props) {
 
@@ -9,6 +14,86 @@ function SignUp(props) {
     props.listenToSignUpPage();
   };
  
+  const [signupObj, setSignupObj] = React.useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+  const [errorObj, setErrorObj] = React.useState({
+    fullNameError: false,
+    fullNameHelper: "",
+    emailError: false,
+    emailHelper: "",
+    passwordError: false,
+    passwordHelper: "",
+  });
+
+  const takefullName = (a) => {
+    setSignupObj((prevState) => ({ ...prevState, fullname: a.target.value }));
+  };
+  const takeEmail = (event) => {
+    setSignupObj((prevState) => ({ ...prevState, email: event.target.value }));
+  };
+  const takePassword = (e) => {
+    setSignupObj((prevState) => ({ ...prevState, password: e.target.value }));
+  };
+
+  const submit = async (event) => {
+    event.preventDefault()
+    console.log("submit triggered")
+    let fullNameTest = nameReg.test(signupObj.fullname);
+    let emailTest = emailRegex.test(signupObj.email);
+    let passwordTest = passwordRegex.test(signupObj.password);
+
+    if (fullNameTest === false) {
+      setErrorObj((prevState) => ({
+        ...prevState,
+        fullNameError: true,
+        fullNameHelper: "enter correct firstname",
+      }));
+    } else {
+      setErrorObj((prevState) => ({
+        ...prevState,
+        fullNameError: false,
+        fullNameHelper: "",
+      }));
+    }
+
+    if (emailTest === false) {
+      setErrorObj((prevState) => ({
+        ...prevState,
+        emailError: true,
+        emailHelper: "enter correct email",
+      }));
+    } else {
+      setErrorObj((prevState) => ({
+        ...prevState,
+        emailError: false,
+        emailHelper: "",
+      }));
+    }
+    if (passwordTest === false) {
+      setErrorObj((prevState) => ({
+        ...prevState,
+        passwordError: true,
+        passwordHelper: "enter correct password",
+      }));
+    } else {
+      setErrorObj((prevState) => ({
+        ...prevState,
+        passwordError: false,
+        passwordHelper: "",
+      }));
+    }
+
+    if (
+      true
+    ) {
+      console.log("prepared for backend",signupObj)
+      let response = await registerUser(signupObj);
+      console.log(response);
+    }
+  }
   return (
     <div className="signin-page">
       <div className="container">
@@ -31,22 +116,31 @@ function SignUp(props) {
                   name="name"
                   className="control"
                   placeholder="Enter Full Name..."
+                  onChange={takefullName}
+                  error={errorObj.fullNameError}
+                  helperText={errorObj.fullNameHelper}
                 />
               </div>
-              <div class="text-box">
+              <div className="text-box">
                 <input
                   type="email"
                   name="name"
                   className="control"
                   placeholder="Enter Email..."
+                  onChange={takeEmail}
+                  error={errorObj.emailError}
+                  helperText={errorObj.emailHelper}
                 />
               </div>
-              <div class="text-box">
+              <div className="text-box">
                 <input
                   type="password"
                   name="name"
                   className="control"
                   placeholder="Enter Password..."
+                  onChange={takePassword}
+                  error={errorObj.passwordError}
+                  helperText={errorObj.passwordHelper}
                 />
               </div>
 
@@ -56,6 +150,7 @@ function SignUp(props) {
                     name="name"
                     className="btn"
                     value="Create account"
+                    onClick={submit}
                   />   
               </div>
               <div className="Login-box"><h4>Already have an Account? <Button onClick={clickLogin}>Login</Button> </h4></div>
--- chat_frontend/src/service/userservice.js
+++ chat_frontend/src/service/userservice.js
@@ -3,7 +3,11 @@ const url = 'http://localhost:6060/api/v1/'
 
 export const registerUser = async(obj) =>{
     console.log(obj);
-    let response  = await axios.post(url + "users", obj)
+    try{
+    let response  = await axios.post(url + "users/", obj)
     console.log(response);
     return response;
+    }catch(err){
+        console.log("err",err)
+    }
 }
